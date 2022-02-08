ae5421e02feab5e59eb2fa7626f52037df4a969c
Velayuthaperumal <VelayuthaPerumal.TrichyRajendran@in.bosch.com>
2022-02-09 00:02:11 +0530

    added single return statement

--- checker.c
+++ checker.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 
+enum status {BAD, GOOD};
 typedef struct bms
 {
 	float actual_value;
@@ -14,27 +15,16 @@ typedef struct bms
 
 int batteryIsOk (BMS battery_params)
 {
+	int battery_status = GOOD;
+	
 	if (battery_params.actual_value < battery_params.limits.min || 
 		battery_params.actual_value > battery_params.limits.max )
 	{
 		printf("out of range!\n");
-		return 0;
+		battery_status = BAD;
 	}
+	return battery_status;
 }
-
-/* int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    printf("Temperature out of range!\n");
-    return 0;
-  } else if(soc < 20 || soc > 80) {
-    printf("State of Charge out of range!\n");
-    return 0;
-  } else if(chargeRate > 0.8) {
-    printf("Charge Rate out of range!\n");
-    return 0;
-  }
-  return 1;
-} */
 
 int main() {
 	float battery_temperature [] = {25.0f, 50.0f};
@@ -45,21 +35,20 @@ int main() {
 	
 	for (reading_index = 0; reading_index < 2; reading_index++)
 	{
-		battery_parameters.limits.min = 0.0f;
-		battery_parameters.limits.max = 45.0f;
+		battery_parameter.limits.min = 0.0f;
+		battery_parameter.limits.max = 45.0f;
 		battery_parameter.actual_value = battery_temperature[reading_index];
 		assert(batteryIsOk(battery_parameter));
 		
-		battery_parameters.limits.min = 20;
-		battery_parameters.limits.max = 80;
+		battery_parameter.limits.min = 20;
+		battery_parameter.limits.max = 80;
 		battery_parameter.actual_value = battery_soc[reading_index];
 		assert(batteryIsOk(battery_parameter));
 		
-		battery_parameters.limits.min = 0.0f;
-		battery_parameters.limits.max = 0.8f;
+		battery_parameter.limits.min = 0.0f;
+		battery_parameter.limits.max = 0.8f;
 		battery_parameter.actual_value = battery_charge_rate[reading_index];
 		assert(batteryIsOk(battery_parameter));
 	}
-	/* assert(batteryIsOk(25, 70, 0.7));
-	assert(!batteryIsOk(50, 85, 0)); */
+	
 }
