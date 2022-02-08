048511bba045664c810205f143a84298bcc33e7b
Velayuthaperumal <VelayuthaPerumal.TrichyRajendran@in.bosch.com>
2022-02-09 00:55:09 +0530

    handled both positve and negative test case

--- checker.c
+++ checker.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 
 enum status {BAD, GOOD};
+const int total_parameter = 3;
 typedef struct bms
 {
 	float actual_value;
@@ -16,39 +17,32 @@ typedef struct bms
 int batteryIsOk (BMS battery_params)
 {
 	int battery_status = GOOD;
-	
+
 	if (battery_params.actual_value < battery_params.limits.min || 
 		battery_params.actual_value > battery_params.limits.max )
 	{
 		printf("out of range!\n");
 		battery_status = BAD;
 	}
+	
 	return battery_status;
 }
 
 int main() {
-	float battery_temperature [] = {25.0f, 50.0f};
-	int battery_soc [] = {70, 85};
-	float battery_charge_rate [] = {0.7f, 0.0f};
-	BMS battery_parameter;
-	int reading_index;
+	float battery_param_in_range [] = {25.0f, 70, 0.7f};
+	float battery_param_out_of_range [] = {50.0f, 85, 0.0f};
+	BMS battery_parameter[total_parameter];
+	int parameter_count;
+	float limit_min [] = {0.0f, 20, 0.0f };
+	float limit_max [] = {45.0f, 80, 0.8f};
 	
-	for (reading_index = 0; reading_index < 2; reading_index++)
+	for (parameter_count = 0; parameter_count < total_parameter; parameter_count++)
 	{
-		battery_parameter.limits.min = 0.0f;
-		battery_parameter.limits.max = 45.0f;
-		battery_parameter.actual_value = battery_temperature[reading_index];
-		assert(batteryIsOk(battery_parameter));
-		
-		battery_parameter.limits.min = 20;
-		battery_parameter.limits.max = 80;
-		battery_parameter.actual_value = battery_soc[reading_index];
-		assert(batteryIsOk(battery_parameter));
-		
-		battery_parameter.limits.min = 0.0f;
-		battery_parameter.limits.max = 0.8f;
-		battery_parameter.actual_value = battery_charge_rate[reading_index];
-		assert(batteryIsOk(battery_parameter));
+		battery_parameter[parameter_count].limits.min = limit_min[parameter_count];
+		battery_parameter[parameter_count].limits.max = limit_max[parameter_count];
+		battery_parameter[parameter_count].actual_value = battery_param_in_range[parameter_count];
+		assert(batteryIsOk(battery_parameter[parameter_count]));
+		battery_parameter[parameter_count].actual_value = battery_param_out_of_range[parameter_count];
+		assert(!batteryIsOk(battery_parameter[parameter_count]));
 	}
-	
 }
