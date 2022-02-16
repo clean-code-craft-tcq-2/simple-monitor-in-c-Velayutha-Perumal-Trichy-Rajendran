aee51e9422bb1a3ef23922d04fd1c197a31ce375
Velayuthaperumal <VelayuthaPerumal.TrichyRajendran@in.bosch.com>
2022-02-16 22:34:45 +0530

    add differemt warning level support

--- checker.c
+++ checker.c
@@ -11,12 +11,12 @@ char *warning_message = "auber Reichweite!!!";
 #endif
 
 struct bms battery_parameter[TOTAL_DATA_SETS] = {
-													{25.0f, 50, 0.5f},
-													{50.0f, 70, 0.7f},
-													{-1.0f, 30, 0.6f},
-													{30.0f, 90, 0.4f},
-													{40.0f, 10, 0.3f},
-													{20.0f, 45, 0.9f}
+													{25.0f, 10, 0.5f},
+													{50.0f, 20, 0.7f},
+													{1.0f, 15, 0.6f},
+													{40.0f, 27, 0.3f},
+													{20.0f, 45, 0.9f},
+													{43.0f, 25, 0.7}
 												};
 
 int readBatteryParameters (int inputDataSetIndex)
@@ -28,32 +28,39 @@ int readBatteryParameters (int inputDataSetIndex)
 	return inputDataSetIndex;
 }
 
-int temperatureOutOfRange ()
+int temperatureOutOfRange (int warning_level)
 {
-	if (g_battery_parameter.temperature < TEMP_MIN || 
-		g_battery_parameter.temperature > TEMP_MAX)
+	if (g_battery_parameter.temperature > temperatureAlertRange[warning_level].min && 
+		g_battery_parameter.temperature <= temperatureAlertRange[warning_level].max)
 	{
 		parameter_alerts[TEMPERATURE].breached = ASSERTED;
+		parameter_alerts[TEMPERATURE].warning_lvl = warning_level;
+		parameter_alerts[TEMPERATURE].warning_type = temperatureAlertRange[warning_level].warning_type;
 	} 
 	 return parameter_alerts[TEMPERATURE].breached;
 }
 
-int socOutOfRange ()
+int socOutOfRange (int warning_level)
 {
-	if (g_battery_parameter.soc < SOC_MIN || 
-		g_battery_parameter.soc > SOC_MAX)
+	if (g_battery_parameter.soc > socAlertRange[warning_level].min && 
+		g_battery_parameter.soc <= socAlertRange[warning_level].max)
 	{
 		parameter_alerts[SOC].breached = ASSERTED;
+		parameter_alerts[SOC].warning_lvl = warning_level;
+		parameter_alerts[SOC].warning_type = socAlertRange[warning_level].warning_type;
 	}
 	
 	return parameter_alerts[SOC].breached;
 }
 
-int chargeRateExceedLimit ()
+int chargeRateExceedLimit (int warning_level)
 {
-	if (g_battery_parameter.chargeRate > CHARGERATE_LIMIT)
+	if (g_battery_parameter.chargeRate > chargeRateAlertRange[warning_level].min &&
+		g_battery_parameter.chargeRate <= chargeRateAlertRange[warning_level].max)
 	{
 		parameter_alerts[CHARGE_RATE].breached = ASSERTED;
+		parameter_alerts[CHARGE_RATE].warning_lvl = warning_level;
+		parameter_alerts[CHARGE_RATE].warning_type = chargeRateAlertRange[warning_level].warning_type;
 	}
 	return parameter_alerts[CHARGE_RATE].breached;
 }
@@ -64,7 +71,7 @@ int batteryIsOk ()
 	
 	for (parameter_count = TEMPERATURE; parameter_count < TOTAL_PARAMETER; parameter_count++)
 	{
-		status |= parameter_alerts[parameter_count].parameterOutOfRange();
+		status |= parameter_alerts[parameter_count].parameterOutOfRange( parameter_alerts[parameter_count].warning_lvl );
 	}
 	return status;
 }
@@ -75,9 +82,10 @@ void printAlertToConsoleIfBreached ()
 	
 	for (parameter_count = TEMPERATURE; parameter_count < TOTAL_PARAMETER; parameter_count++)
 	{
-		if (parameter_alerts[parameter_count].breached)
+		if (parameter_alerts[parameter_count].breached && 
+			parameter_alerts[parameter_count].warning_lvl != NORMAL)
 		{
-			printf("%s %s\n", parameter_alerts[parameter_count].parameter_name, warning_message);
+			printf("%s %s: %s\n", parameter_alerts[parameter_count].parameter_name, warning_message, parameter_alerts[parameter_count].warning_type);
 		}		
 	}
 }
@@ -91,13 +99,16 @@ void resetOldStatus ()
 
 int main() {
 	
-	int data_set = FIRST;
+	int data_set;
 	
-	readBatteryParameters(data_set);
-	assert(batteryIsOk() == GOOD);
-	printAlertToConsoleIfBreached ();
-	resetOldStatus();
-	for (data_set = SECOND; data_set < TOTAL_DATA_SETS; data_set++)
+	for (data_set = FIRST; data_set < TOTAL_POSITIVE_DATA_SETS; data_set++)
+	{
+		readBatteryParameters(data_set);
+		assert(batteryIsOk() == GOOD);
+		printAlertToConsoleIfBreached ();
+		resetOldStatus();
+	}
+	for (data_set; data_set < TOTAL_DATA_SETS; data_set++)
 	{
 		readBatteryParameters(data_set);
 		assert(batteryIsOk() == BAD);
--- checker.h
+++ checker.h
@@ -1,24 +1,29 @@
 #ifndef __CHECKER_H_
 #define __CHECKER_H_
 
-#define TEMP_MIN			0.0f
+/* #define TEMP_MIN			0.0f
 #define TEMP_MAX			45.0f
 #define SOC_MIN				20
 #define SOC_MAX				80
-#define CHARGERATE_LIMIT	0.8f
-#define TOTAL_PARAMETER		3
-#define TOTAL_DATA_SETS     6
+#define CHARGERATE_LIMIT	0.8f */
+#define TOTAL_PARAMETER				 3
+#define TOTAL_POSITIVE_DATA_SETS     3
+#define TOTAL_DATA_SETS     		 6
+#define NORMAL						 2					
 
 enum battery_conditon {GOOD, BAD};
 enum dataSet {FIRST, SECOND};
 enum alertStatus {NOT_ASSERTED, ASSERTED};
 enum params_list {TEMPERATURE, SOC, CHARGE_RATE};
 enum rangeType {IN_RANGE, TEST_ID_01};
+enum TemperatureWarningLevel{LOW_TEMP_BREACH, LOW_TEMP_WARNING, NORMAL_TEMP, HIGH_TEMP_WARNING, HIGH_TEMP_BREACH};
+enum socWarningLevel{LOW_SOC_BREACH, LOW_SOC_WARNING, NORMAL_SOC, HIGH_SOC_WARNING, HIGH_SOC_BREACH};
+enum chargeRateWarningLevel{LOW_CR_BREACH, LOW_CR_WARNING, NORMAL_CR, HIGH_CR_WARNING, HIGH_CR_BREACH};
 
 /*Parameter validator Function declaration */
-int temperatureOutOfRange ();
-int socOutOfRange ();
-int chargeRateExceedLimit ();
+int temperatureOutOfRange (int warning_lvl);
+int socOutOfRange (int warning_lvl);
+int chargeRateExceedLimit (int warning_lvl);
 
 struct bms
 {
@@ -27,18 +32,56 @@ struct bms
 	float chargeRate;
 }g_battery_parameter;
 
-typedef int (*isParamterNormal)(void);
+typedef int (*isParamterNormal)(int);
 
 struct alert
 {
 	int breached;
 	char *parameter_name;
 	isParamterNormal parameterOutOfRange;
+	int warning_lvl;
+	char *warning_type;
 } parameter_alerts[TOTAL_PARAMETER] = 	{
-											{NOT_ASSERTED, "TEMPERATURE", temperatureOutOfRange},
-											{NOT_ASSERTED, "SOC", socOutOfRange},
-											{NOT_ASSERTED, "CHARGE_RATE", chargeRateExceedLimit}
+											{NOT_ASSERTED, "TEMPERATURE", temperatureOutOfRange, HIGH_TEMP_WARNING, NULL},
+											{NOT_ASSERTED, "SOC", socOutOfRange, NORMAL_SOC, NULL},
+											{NOT_ASSERTED, "CHARGE_RATE", chargeRateExceedLimit, HIGH_CR_BREACH, NULL}
 										};
 
-
+struct socAlerts
+{
+	int min;
+	int max;
+	char *warning_type;
+} socAlertRange[5] =	{
+							{0, 20, "LOW_SOC_BREACH"},
+							{20, 24, "LOW_SOC_WARNING"},
+							{24, 75, "NORMAL_SOC"},
+							{75, 80, "HIGH_TEMP_WARNING"},
+							{80, 100, "HIGH_TEMP_BREACH"}
+						};
+struct temperatureAlerts
+{
+	float min;
+	float max;
+	char *warning_type;
+}temperatureAlertRange[5] = {
+								{0.0f, 5.0f, "LOW_TEMP_BREACH"},
+								{5.0f, 10.0f, "LOW_TEMP_WARNING"},
+								{10.0f, 40.0f, "NORMAL_TEMP"},
+								{40.0f, 45.0f, "HIGH_TEMP_WARNING"},
+								{45.0f, 60.0f, "HIGH_TEMP_BREACH"}
+							};
+							
+struct chargeRateAlerts 
+{
+	float min;
+	float max;
+	char *warning_type;
+}chargeRateAlertRange[5] = {
+									{0.0f, 0.4f, "LOW_CR_BREACH"},
+									{0.4f, 0.6f, "LOW_CR_WARNING"},
+									{0.6f, 0.8f, "NORMAL_CR"},
+									{0.8f, 0.9f, "HIGH_CR_WARNING"},
+									{0.8f, 1.0f, "HIGH_CR_BREACH"}
+							};
 #endif
